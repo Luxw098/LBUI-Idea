e2fb62ad5d02a631de33d36447bbaa4fd9133d79
Lux <152927191+Luxw098@users.noreply.github.com>
2025-04-25 00:34:26 +0100

    Minor updates

--- src/public/index.js
+++ src/public/index.js
@@ -17,12 +17,12 @@ addEventListener("DOMContentLoaded", () => {
     document.body.style.overflow = "hidden";
     console.log(2)
     const overlay = document.getElementById("overlay");
-    const sidebar = document.getElementById("sidebar");
-    const topbar = document.getElementById("topbar");
+    const account = document.getElementById("account");
+    const header = document.getElementById("header");
     const content = document.getElementById("content");
 
-    enter_animation(sidebar, ["-100%", "0%"], ["0%", "0%"]);
-    enter_animation(topbar, ["0", "0%"], ["-100%", "0%"]);
+    enter_animation(account, ["-100%", "0%"], ["0%", "0%"]);
+    enter_animation(header, ["0", "0%"], ["-100%", "0%"]);
     enter_animation(content, ["0%", "0%"], ["120%", "0%"]);
 
     overlay.hidden = true;
@@ -35,11 +35,13 @@ addEventListener("DOMContentLoaded", () => {
 function update_theme(theme) {
     if (theme == "l") {
         document.documentElement.style.setProperty('--color-primary', '#fff');
-        document.documentElement.style.setProperty('--color-secondary', '#ddd');
+        document.documentElement.style.setProperty('--color-secondary', '#eee');
+        document.documentElement.style.setProperty('--color-trimary', '#ddd');
         document.documentElement.style.setProperty('--color-text', '#666');
     } else {
         document.documentElement.style.setProperty('--color-primary', '#222');
         document.documentElement.style.setProperty('--color-secondary', '#333');
+        document.documentElement.style.setProperty('--color-trimary', '#444');
         document.documentElement.style.setProperty('--color-text', '#eee');
     };
 }
